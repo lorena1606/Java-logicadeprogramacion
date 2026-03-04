d8a1c80d9d6652cd793bc04cd319de45e505dd33
Michell lorena Roldan Moncada <michellroldan14@gmail.com>
2026-03-03 21:26:17 -0500

    Añadi ejemplos de casting implicito (widening) y explicito

--- src/main/java/com/example/ConversionDeTipos.java
+++ src/main/java/com/example/ConversionDeTipos.java
@@ -2,6 +2,27 @@ package com.example;
 
 public class ConversionDeTipos {
     public static void demostrarCastingImplicito(){
-        
+        int numeroInt = 100;
+        double numDouble = numeroInt;
+
+        System.out.println("int: " + numeroInt);
+        System.out.println("double: " + numDouble);
+    }
+    public static void demostrarCastingExplicito(){
+        double numeDouble = 9.99;
+        int numInt = (int) numeDouble;
+
+        System.out.println("double:" + numeDouble);
+        System.out.println("int convertido " + numInt);
+
+    }
+    public static void demostrarProblemaDePrecision(){
+        double valorDouble = 7.89;
+        int valorInt = (int) valorDouble;
+
+        System.out.println("Valor original (double): " + valorDouble);
+        System.out.println("Valor convertido (int): " + valorInt);
+        System.out.println("Se pierden los decimales al convertir ");
+
     }
 }
